9dd5e200bb638f09f7134e2f66d01c1e7cc3105f
David Cock <davec@cse.unsw.edu.au>
2013-09-18 20:31:12 +1000

    * Typo

--- monte_carlo.py
+++ monte_carlo.py
@@ -93,7 +93,7 @@ for s in sizes:
     pipes= [subprocess.Popen("%s %s %d %d %d %d < %s > /dev/null" % \
         (channel_matrix, subsampled_channels[i][1], \
          mod_range[0], mod_range[1], s, discards[i], samples_file[1]), \
-        shell=True, stdout=subprocess.PIPE) for i in xrange(len(sizes)]
+        shell=True, stdout=subprocess.PIPE) for i in xrange(len(sizes))]
 
     for p in pipes:
         sodata, sedata= p.communicate()
@@ -101,7 +101,7 @@ for s in sizes:
     # Calculate capacities
     pipes= [subprocess.Popen("%s %s %f -q" % \
         (capacity, subsampled_channels[i][1], epsilon), \
-        shell=True, stdout=subprocess.PIPE) for i in xrange(len(sizes)]
+        shell=True, stdout=subprocess.PIPE) for i in xrange(len(sizes))]
 
     for p in pipes:
         sodata, sedata= p.communicate()
