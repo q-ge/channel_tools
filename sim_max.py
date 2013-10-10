7a854cb450f994bdcd44a9c55e7fd4a070475dbb
David Cock <davec@cse.unsw.edu.au>
2013-10-10 11:42:24 +1100

    * Find the maximum of a sim run

---
+++ sim_max.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+
+ymax= -1
+
+for l in sys.stdin:
+    bits= l.strip().split(' ')
+    if len(bits) == 0: continue
+    if bits[0] == '#': continue
+
+    x, y, ye= map(float, bits)
+
+    if y+ye > ymax: ymax= y+ye
+
+print "%.2e %.12e" % (ymax, ymax)
